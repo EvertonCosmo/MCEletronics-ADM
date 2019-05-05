37bc8c2cd48946d80499eaaf2fbefad84595b4a2
Everton Cosmo <everton8cosmo@gmail.com>
2019-05-05 17:10:35 -0300

    call components composition

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main-bar></main-bar>
+    <home-page></home-page>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import MainBar from "./components/MainBar.vue"
+import HomePage from "./components/HomePage.vue"
 export default {
   name: 'app',
   components: {
-    HelloWorld
+   MainBar,HomePage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
