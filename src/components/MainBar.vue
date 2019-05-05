b47849f1c8e8a462fb2782433dacd7d96c702cb7
Everton Cosmo <everton8cosmo@gmail.com>
2019-05-05 17:08:23 -0300

    fixed bug in size in mobile devices

---
+++ src/components/MainBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <header class="nav-bar">
+        <span class="logo">{{tittle}}</span>
+    </header>
+</template>
+
+
+<<script>
+export default {
+    name:"MainBar",
+
+    data(){
+        return{
+            tittle:"MCEletronics"
+        }
+
+
+    }
+}
+
+</script>
+
+<style scoped>
+
+.nav-bar{
+        background: linear-gradient(-90deg,#033076);
+        box-shadow: 1px 2px 2px 2px #033076;
+        height: 4rem;
+        width: 100%;
+        margin-bottom: 10px;
+        margin:auto;
+        
+    }
+    .logo{
+        color:white;
+       
+        text-decoration: none;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 2.3rem;
+        margin-left: 10px;
+        
+    }
+</style>
